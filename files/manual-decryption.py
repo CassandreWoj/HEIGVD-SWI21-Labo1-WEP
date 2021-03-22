19af76e85d798ffc3d335a313f6a44e27d3c4ac1
gabriel Roch <gabriel.roch@heig-vd.ch>
2021-03-22 18:47:29 +0100

    Commit en retard, sorry 😞

--- files/manual-decryption.py
+++ files/manual-decryption.py
@@ -13,33 +13,49 @@ __status__ 		= "Prototype"
 from scapy.all import *
 import binascii
 from rc4 import RC4
+import zlib
+
 #Cle wep AA:AA:AA:AA:AA
 key= b'\xaa\xaa\xaa\xaa\xaa'
 
 #lecture de message chiffré - rdpcap retourne toujours un array, même si la capture contient un seul paquet
-arp = rdpcap('arp.cap')[0]  
+arp = rdpcap('test.cap')[0]
 
 # rc4 seed est composé de IV+clé
 seed = arp.iv+key
 
 # recuperation de icv dans le message (arp.icv) (en chiffre) -- je passe au format "text". Il y a d'autres manières de faire ceci...
+print("arp.icv     ", hex(arp.icv))
 icv_encrypted='{:x}'.format(arp.icv)
-
+print("icv_enc     ", icv_encrypted)
 # text chiffré y-compris l'icv
 message_encrypted=arp.wepdata+bytes.fromhex(icv_encrypted)
 
+
 # déchiffrement rc4
 cipher = RC4(seed, streaming=False)
-cleartext=cipher.crypt(message_encrypted)
+print("seed:", seed)
+print("mess_encr", message_encrypted)
+cleartext = cipher.crypt(message_encrypted)
+print("payload: ", cleartext)
 
 # le ICV est les derniers 4 octets - je le passe en format Long big endian
 icv_enclair=cleartext[-4:]
-icv_enclair = icv_enclair
+# print("target icv :", hex(zlib.crc32(cleartext[:-4])))
+# print("enclair icv:", icv_enclair)
+# print("payload : ", cleartext[:-4])
+if zlib.crc32(cleartext[:-4]).to_bytes(4, byteorder='little') == icv_enclair:
+    print("ICV OK");
+else:
+    print("ICV PAS OK");
 icv_numerique=struct.unpack('!L', icv_enclair)
 
 # le message sans le ICV
 text_enclair=cleartext[:-4]
 
 print ('Text: ' + text_enclair.hex())
+print ('Text: ' , text_enclair)
 print ('icv:  ' + icv_enclair.hex())
 print ('icv(num): ' + str(icv_numerique))
+print (icv_encrypted)
+
---
+++ files/manual-fragmentation.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" Manually decrypt a wep message given the WEP key"""
+
+__author__      = "Abraham Rubinstein"
+__copyright__   = "Copyright 2017, HEIG-VD"
+__license__ 	= "GPL"
+__version__ 	= "1.0"
+__email__ 		= "abraham.rubinstein@heig-vd.ch"
+__status__ 		= "Prototype"
+
+from scapy.all import *
+import binascii
+from rc4 import RC4
+import zlib
+
+#Nouveau payload
+new_payload=b"\xaa\xaa\x03\x00\x00\x00\x08\x06\x00\x01\x08\x00\x06\x04\x00\x01\x90'\xe4\xeaa\xf2\xc0\xa8\x01d\x00\x00\x00\x00\x00\x00\xc0\xa8\x01\xc8"
+new_payload=b"\xaa\xaa\x03\x00\x00\x00\x08\x06\x00\x01\x08\x00\x06\x04\x00\x01\x92'\xe4\xeaa\xf2\xc0\xa8\x01d\x00\x00\x00\x00\x00\x00\xc0\xa8\x01\xc8"
+
+#Cle wep AA:AA:AA:AA:AA
+key= b'\xaa\xaa\xaa\xaa\xaa'
+
+#lecture de message chiffré - rdpcap retourne toujours un array, même si la capture contient un seul paquet
+arp = rdpcap('arp.cap')[0]
+
+# rc4 seed est composé de IV+clé
+seed = arp.iv+key
+
+# recuperation de icv dans le message (arp.icv) (en chiffre) -- je passe au format "text". Il y a d'autres manières de faire ceci...
+icv_encrypted='{:x}'.format(arp.icv)
+
+# text chiffré y-compris l'icv
+message_encrypted=arp.wepdata+bytes.fromhex(icv_encrypted)
+
+
+
+# déchiffrement rc4
+cipher = RC4(seed, streaming=False)
+icv_pre_calc = zlib.crc32(new_payload)
+new_full_payload = new_payload+icv_pre_calc.to_bytes(4, byteorder='little');
+new_full_payload = cipher.crypt(new_full_payload)
+
+
+fragements = [0] * 4
+nb_fragm = 4
+frag_no = 0
+for i in range(0, len(new_full_payload), int(len(new_full_payload)/nb_fragm)):
+    e = int(i+len(new_full_payload)/nb_fragm)
+    fragment_payload = new_full_payload[i:e]
+
+    arp.SC += 0
+    arp.FCfield |= 0x04
+    icv_pre_calc = zlib.crc32(fragment_payload)
+    new_fragment_payload = fragment_payload + icv_pre_calc.to_bytes(4, byteorder='little');
+    new_fragment_payload = cipher.crypt(new_fragment_payload)
+    arp.wepdata = new_fragment_payload[:-4]
+    arp.icv = int.from_bytes(new_fragment_payload[-4:], 'big')
+    # print(frag_no)
+    print(hex(int.from_bytes(new_fragment_payload[:-4], byteorder='big')),
+          hex(int.from_bytes(new_fragment_payload[-4:], byteorder='big')))
+    fragements[frag_no] = arp.copy()
+    frag_no += 1
+
+    pass
+print(len(new_full_payload) / 4)
+fragements[nb_fragm-1].FCfield &= ~0x04
+
+# Passé au fragement suivant
+arp.SC += 0
+
+# Définit le bit more fragment à 1
+arp.FCfield |= 0x04;
+
+
+arp.wepdata = new_full_payload[:-4]
+arp.icv = int.from_bytes(new_full_payload[-4:], 'big');
+# cleartext = cipher.crypt(message_encrypted)
+#
+# le ICV est les derniers 4 octets - je le passe en format Long big endian
+# icv_enclair=cleartext[-4:]
+# print(zlib.crc32(cleartext[:-4]).to_bytes(4, byteorder='little'))
+
+# le message sans le ICV
+# text_enclair=cleartext[:-4]
+
+
+wrpcap("test.cap", fragements);
+# wrpcap("test.cap", [arp, arp]);
